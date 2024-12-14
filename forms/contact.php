094f731c82d7560d17180930bdcef9aa179eae80
Dan Sidsaya <dansidsaya@gmail.com>
2024-12-14 12:26:51 +0000

    updates today

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,5 @@
 <?php
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'info@tekcycle.com';
 
   if( file_exists($php_email_form = '../images/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,15 +15,15 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  // SMTP configuration for sending emails
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    'host' => 'smtp.gmail.com', 
+    'username' => 'dansidsaya@gmail.com', 
+    'password' => 'ezal mhdm bieo qwpp', 
+    'port' => '456', 
+    'encryption' => 'ssl'
+);
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
